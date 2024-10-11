8fa2ce27729f95e3cabdc06463f0151828250fa8
Aruthra <aruthrasri8@gmail.com>
2024-10-11 15:22:21 +0530

    Update service11_main.py with session name

--- service11_main.py
+++ service11_main.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 import os
 import datetime
 import general as gen
-import uds_dummy_1 as uds     #will have to be replaced with actual uds file while testing on board
+import uds_dummy as uds     #will have to be replaced with actual uds file while testing on board
 #pc#import uds
 import configure as conf
 import os
@@ -57,7 +57,11 @@ class Ui_Service11(Ui_Form_SID11):
     def send11service(self):
         index_ERSession = self.comboBox_ECUReset.currentIndex()
         session = fun.getsubfunction(index_ERSession)
+        session_name = fun.getsubfunctionname(session)
         sprmib_flg = self.checkBox_suppressposmsg.isChecked()
+        
+
+        
 
         #session should be a valid value and not zero
         if(0 == session):
@@ -65,7 +69,7 @@ class Ui_Service11(Ui_Form_SID11):
             gen.log_action("UDS Request Fail", f"11 Request not happened due to invalid Reset Type selection [{self.comboBox_ECUReset.currentText()}]")
             return
         
-        #Get the service Request List for Diagnostic Session Control
+        #Get the service Request List for ECU Reset service
         service_request = fun.form_reqmsg4srv11(session,sprmib_flg)
 
         #Send the service request and get the response 
@@ -80,13 +84,15 @@ class Ui_Service11(Ui_Form_SID11):
         gen.log_action("UDS Request Success", f"11 Request Successfully sent : {' '.join(hex(number) for number in service_request)}")
 
         if(response.type == "Positive Response"):
-            #p2servermax = ((response.resp[2] << 8)|(response.resp[3]))
-            #p2starservermax = ((response.resp[4] << 8)|(response.resp[5]))
+            p2servermax = ((response.resp[2] << 8)|(response.resp[3]))
+            p2starservermax = ((response.resp[4] << 8)|(response.resp[5]))
 
             response_html = f'''<h4><U>Positive Response Recieved</U></h4>
     <p><strong>Service ID:</strong> <I>{hex(response.resp[0]-0x40)}</I></p>
     <p><strong>Reset Type:</strong> <I>{hex(response.resp[1])}</I></p>
     <p><strong>Suppress Positive Message Request:</strong> <I>{sprmib_flg}</I></p>
+    <p><strong>P2ServerMax:</strong> <I>{p2servermax} milliseconds</I></p>
+    <p><strong>P2*ServerMax:</strong> <I>{p2starservermax} milliseconds</I></p>
     
 '''
 
@@ -122,7 +128,7 @@ class Ui_Service11(Ui_Form_SID11):
 
         self.logentrystring = f'''<---- LOG ENTRY [{current_user} - {currenttime}] ---->
 UDS Request :   [{" ".join(hex(number) for number in service_request)}]
-Explaination:   ECU Reset (Service 11) Requested for session 0x{session} and SPRMIB flag {sprmib_flg}
+Explaination:   ECU Reset (Service 11) Requested for reset type 0x{session} ({session_name}) and SPRMIB flag {sprmib_flg}
 UDS Response:   [{" ".join(hex(number) for number in response.resp)}]
 Explaination:   {response_text}<------------------- LOG ENTRY END ------------------->
 
