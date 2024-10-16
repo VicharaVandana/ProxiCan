2f0a4e969b23a24b1b76ee3973b134390ec87b59
Srinivasan <ass930085@tatatechnologies.com>
2024-10-16 10:09:48 +0530

    Update uds_dummy.py

--- uds_dummy.py
+++ uds_dummy.py
@@ -160,13 +160,7 @@ def sendRequest(request, IsPosResExpected = True):
             response.nrc = 0x00
             response.nrcname = NRC_DATA.get(response.nrc, "Unknown NRC")[0]
             response.nrcdesc = NRC_DATA.get(response.nrc, "Unknown NRC Recieved. There is no record of this NRC in UDS ISO 14229 Document")[1]
-        elif(request[1] == 0x02):
-            res = [0x7F, 0x11, 0x22]
-            response.resp = res.copy()
-            response.type = "Negative Response"
-            response.nrc = 0x22
-            response.nrcname = NRC_DATA.get(response.nrc, "Unknown NRC")[0]
-            response.nrcdesc = NRC_DATA.get(response.nrc, "Unknown NRC Recieved. There is no record of this NRC in UDS ISO 14229 Document")[1]
+    
         else:
             res = [0x7F, 0x11, 0x12]
             response.resp = res.copy()
