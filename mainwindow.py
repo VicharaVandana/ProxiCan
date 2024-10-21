cdadb2cdb381a999f9198450e45597b62c47d6c4
Srinivasan <ass930085@tatatechnologies.com>
2024-10-21 12:16:20 +0530

    Service 11 EcuReset Implemented

--- mainwindow.py
+++ mainwindow.py
@@ -14,6 +14,7 @@ import service22_main as rdbi
 import service2e_main as wdbi
 import service10_main as dsc
 import service27_main as secuacc
+import service11_main as er
 
 Is_CanConnected = False
 
@@ -128,6 +129,7 @@ class mainwindow(Ui_MainWindow, QtWidgets.QWidget):
         self.pushButton_2E.clicked.connect(self.openservice2E)
         self.pushButton_10.clicked.connect(self.openservice10)
         self.pushButton_27.clicked.connect(self.openservice27)
+        self.pushButton_11.clicked.connect(self.openservice11)
         return
     
     def on_radiobutton_fdf_clicked(self):
@@ -182,6 +184,20 @@ class mainwindow(Ui_MainWindow, QtWidgets.QWidget):
             gen.log_action("Button Click", "Service 27 Window Opened")
         else:
             self.update_status(f"27 Button clicked. But unable to open the service window as CAN is not connected")
+        return
+    
+    def openservice11(self):
+        if(Is_CanConnected == True):
+            self.window11 = QMainWindow()
+            self.ui11 = er.Ui_Service11()
+            self.ui11.setupUi(self.window11)
+            self.ui11.redesign_ui()
+            self.ui11.connectFunctions()
+            self.window11.show()  # Display the new window
+            self.update_status(f"11 Button clicked. ECU Reset service window opened")
+            gen.log_action("Button Click", "Service 11 Window Opened")
+        else:
+            self.update_status(f"11 Button clicked. But unable to open the service window as CAN is not connected")
         return
     
     def openservice22(self):
