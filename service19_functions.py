a85da6fbc3a337ad40bd943bd566773cdab03fab
K <vkk930175@tatatechnologies.com>
2024-11-15 11:31:09 +0530

    Updated UI files service 19 for subfunctions with 07 and 08

--- service19_functions.py
+++ service19_functions.py
@@ -1,58 +1,8 @@
 import re
-
-def get_subfunction(index):
-     if(index == 0):
-        return(0x01)
-     elif(index == 1):
-        return(0x02)
-     elif(index == 2):
-        return(0x04)
-     elif(index == 3):
-        return(0x06)
-     elif(index == 4):
-        return(0x07)
-     elif(index == 5):
-        return(0x08)
-     elif(index == 6):
-        return(0x09)
-     elif(index == 7):
-        return(0x0A)
-     elif(index == 8):
-        return(0x0C)
-     elif(index == 9):
-        return(0x0E)
-     # elif(index == 10):
-     #    return([0x14,0xFF,0xFF,0xFF])
-     else:
-          return(0x00)
-     
-def getsubfunction_name(subfun):
-     if(subfun == 0x01):
-          return("Report Number of DTC by Status Mask")
-     elif(subfun == 0x02) :
-          return("Report DTC by Status Mask")
-     elif(subfun == 0x04):
-          return("Report DTC Snapshot Record by DTC Number") 
-     elif(subfun == 0x06):
-          return("Report DTC ExtData Record by DTC Number")
-     elif(subfun == 0x07):
-          return("Report Number of DTC by Severity Mask Record")
-     elif(subfun == 0x08):
-          return("Report DTC by Severity Mask Record")
-     elif(subfun == 0x09) :
-          return("Report Severity Information of DTC")
-     elif(subfun == 0x0A):
-          return("Report Supported DTC") 
-     elif(subfun == 0x0C):
-          return("Report First Confirmed DTC")
-     elif(subfun == 0x0E):
-          return("Report Most Recent Confirmed DTC")
-     # elif(subfun == [0x14,0xFF,0xFF,0xFF]):
-     #      return("Fault Memory Clear")
-
-     
-def form_reqmsg4srv19_subfun_3_4(subfun,DTCMaskRecord,DTCSnapshotRecordNumber,sprmib_flag): 
+    
+def form_reqmsg4srv19_subfun_3(DTCMaskRecord,DTCSnapshotRecordNumber,sprmib_flag): 
     sid = int("19", 16)
+    subfun=int("3",16)
     if (sprmib_flag == True):
          subfunction = int(subfun) | 0x80   #MSB is set if SPRMIB is requested.
     else:
@@ -67,8 +17,39 @@ def form_reqmsg4srv19_subfun_3_4(subfun,DTCMaskRecord,DTCSnapshotRecordNumber,sp
     print(f"{sid} {subfunction} {DTC_highbyte} {DTC_middlebyte} {DTC_lowbyte} {DTCSnapshotRecordNumber_hex}")
     return(req_bytes)
 
-def form_reqmsg4srv19_subfun_6_10(subfun,DTCMaskRecord,DTCExtDataRecordNumber,sprmib_flag): 
+def form_reqmsg4srv19_subfun_4(DTCMaskRecord,DTCSnapshotRecordNumber,sprmib_flag): 
     sid = int("19", 16)
+    subfun=int("4",16)
+    if (sprmib_flag == True):
+         subfunction = int(subfun) | 0x80   #MSB is set if SPRMIB is requested.
+    else:
+         subfunction = int(subfun)   
+    DTCMaskRecord_without_spaces = re.sub(r"\s+", "", DTCMaskRecord)
+    DTC_highbyte = int(DTCMaskRecord_without_spaces[:2], 16)
+    DTC_middlebyte = int(DTCMaskRecord_without_spaces[2:4], 16)
+    DTC_lowbyte = int(DTCMaskRecord_without_spaces[4:], 16)
+    DTCSnapchotRecordNumber_without_spaces = re.sub(r"\s+", "", DTCSnapshotRecordNumber)
+    DTCSnapshotRecordNumber_hex = int(DTCSnapchotRecordNumber_without_spaces, 16)
+    req_bytes = [sid, subfunction, DTC_highbyte,DTC_middlebyte,DTC_lowbyte,DTCSnapshotRecordNumber_hex]
+    print(f"{sid} {subfunction} {DTC_highbyte} {DTC_middlebyte} {DTC_lowbyte} {DTCSnapshotRecordNumber_hex}")
+    return(req_bytes)
+
+def form_reqmsg4srv19_subfun_5(DTCStoredDataRecordNumber,sprmib_flag): 
+    sid = int("19", 16)
+    subfun=int("5",16)
+    if (sprmib_flag == True):
+         subfunction = int(subfun) | 0x80   #MSB is set if SPRMIB is requested.
+    else:
+         subfunction = int(subfun)   
+    DTCStoredDataRecordNumber_without_spaces = re.sub(r"\s+", "", DTCStoredDataRecordNumber)
+    DTCStoredDataRecordNumber_hex = int(DTCStoredDataRecordNumber_without_spaces, 16)
+    req_bytes = [sid, subfunction, DTCStoredDataRecordNumber_hex]
+    print(f"{sid} {subfunction} {DTCStoredDataRecordNumber_hex}")
+    return(req_bytes)
+
+def form_reqmsg4srv19_subfun_6(DTCMaskRecord,DTCExtDataRecordNumber,sprmib_flag): 
+    sid = int("19", 16)
+    subfun=int("6",16)
     if (sprmib_flag == True):
          subfunction = int(subfun) | 0x80   #MSB is set if SPRMIB is requested.
     else:
@@ -83,8 +64,26 @@ def form_reqmsg4srv19_subfun_6_10(subfun,DTCMaskRecord,DTCExtDataRecordNumber,sp
     print(f"{sid} {subfunction} {DTC_highbyte} {DTC_middlebyte} {DTC_lowbyte} {DTCExtDataRecordNumber_hex}")
     return(req_bytes)
 
-def form_reqmsg4srv19_subfun_7_8(subfun,DTCSeverityMaskRecord,sprmib_flag): 
+def form_reqmsg4srv19_subfun_10(DTCMaskRecord,DTCExtDataRecordNumber,sprmib_flag): 
     sid = int("19", 16)
+    subfun=int("10",16)
+    if (sprmib_flag == True):
+         subfunction = int(subfun) | 0x80   #MSB is set if SPRMIB is requested.
+    else:
+         subfunction = int(subfun)   
+    DTCMaskRecord_without_spaces = re.sub(r"\s+", "", DTCMaskRecord)
+    DTC_highbyte = int(DTCMaskRecord_without_spaces[:2], 16)
+    DTC_middlebyte = int(DTCMaskRecord_without_spaces[2:4], 16)
+    DTC_lowbyte = int(DTCMaskRecord_without_spaces[4:], 16)
+    DTCExtDataRecordNumber_without_spaces = re.sub(r"\s+", "", DTCExtDataRecordNumber)
+    DTCExtDataRecordNumber_hex = int(DTCExtDataRecordNumber_without_spaces, 16)
+    req_bytes = [sid, subfunction, DTC_highbyte,DTC_middlebyte,DTC_lowbyte,DTCExtDataRecordNumber_hex]
+    print(f"{sid} {subfunction} {DTC_highbyte} {DTC_middlebyte} {DTC_lowbyte} {DTCExtDataRecordNumber_hex}")
+    return(req_bytes)
+
+def form_reqmsg4srv19_subfun_7(DTCSeverityMaskRecord,sprmib_flag): 
+    sid = int("19", 16)
+    subfun=int("7",16)
     if (sprmib_flag == True):
          subfunction = int(subfun) | 0x80   #MSB is set if SPRMIB is requested.
     else:
@@ -96,8 +95,23 @@ def form_reqmsg4srv19_subfun_7_8(subfun,DTCSeverityMaskRecord,sprmib_flag):
     print(f"{sid} {subfunction} {DTC_highbyte} {DTC_lowbyte} ")
     return(req_bytes)
 
-def form_reqmsg4srv19_subfun_9(subfun,DTCMaskRecord,sprmib_flag): 
+def form_reqmsg4srv19_subfun_8(DTCSeverityMaskRecord,sprmib_flag): 
     sid = int("19", 16)
+    subfun=int("8",16)
+    if (sprmib_flag == True):
+         subfunction = int(subfun) | 0x80   #MSB is set if SPRMIB is requested.
+    else:
+         subfunction = int(subfun)   
+    DTCSeverityMaskRecord_without_spaces = re.sub(r"\s+", "", DTCSeverityMaskRecord)
+    DTC_highbyte = int(DTCSeverityMaskRecord_without_spaces[:2], 16)
+    DTC_lowbyte = int(DTCSeverityMaskRecord_without_spaces[2:], 16)
+    req_bytes = [sid, subfunction, DTC_highbyte,DTC_lowbyte]
+    print(f"{sid} {subfunction} {DTC_highbyte} {DTC_lowbyte} ")
+    return(req_bytes)
+
+def form_reqmsg4srv19_subfun_9(DTCMaskRecord,sprmib_flag): 
+    sid = int("19", 16)
+    subfun=int("9",16)
     if (sprmib_flag == True):
          subfunction = int(subfun) | 0x80   #MSB is set if SPRMIB is requested.
     else:
@@ -122,8 +136,9 @@ def form_reqmsg4srv19_subfun_1_2_0F_11_12_13(subfun,DTCStatusMask,sprmib_flag):
     print(f"{sid} {subfunction} {DTCStatusMask_hex}")
     return(req_bytes)
 
-def form_reqmsg4srv19_subfun_A_B_C_D_E_14_15(subfun,sprmib_flag): 
+def form_reqmsg4srv19_subfun_A(sprmib_flag): 
     sid = int("19", 16)
+    subfun=int("0A",16)
     if (sprmib_flag == True):
          subfunction = int(subfun) | 0x80   #MSB is set if SPRMIB is requested.
     else:
@@ -132,11 +147,151 @@ def form_reqmsg4srv19_subfun_A_B_C_D_E_14_15(subfun,sprmib_flag):
     print(f"{sid} {subfunction}")
     return(req_bytes)
 
-# def form_reqmsg4srv19_subfun_clear(sprmib_flag): 
-#     req_bytes = [0x14,0xff,0xff,0xff]
-#     print(req_bytes)
-#     return(req_bytes)
+def form_reqmsg4srv19_subfun_B(sprmib_flag): 
+    sid = int("19", 16)
+    subfun=int("0B",16)
+    if (sprmib_flag == True):
+         subfunction = int(subfun) | 0x80   #MSB is set if SPRMIB is requested.
+    else:
+         subfunction = int(subfun)   
+    req_bytes = [sid, subfunction]
+    print(f"{sid} {subfunction}")
+    return(req_bytes)
 
+def form_reqmsg4srv19_subfun_C(sprmib_flag): 
+    sid = int("19", 16)
+    subfun=int("0C",16)
+    if (sprmib_flag == True):
+         subfunction = int(subfun) | 0x80   #MSB is set if SPRMIB is requested.
+    else:
+         subfunction = int(subfun)   
+    req_bytes = [sid, subfunction]
+    print(f"{sid} {subfunction}")
+    return(req_bytes)
+
+def form_reqmsg4srv19_subfun_D(sprmib_flag): 
+    sid = int("19", 16)
+    subfun=int("0D",16)
+    if (sprmib_flag == True):
+         subfunction = int(subfun) | 0x80   #MSB is set if SPRMIB is requested.
+    else:
+         subfunction = int(subfun)   
+    req_bytes = [sid, subfunction]
+    print(f"{sid} {subfunction}")
+    return(req_bytes)
+
+def form_reqmsg4srv19_subfun_E(sprmib_flag): 
+    sid = int("19", 16)
+    subfun=int("0E",16)
+    if (sprmib_flag == True):
+         subfunction = int(subfun) | 0x80   #MSB is set if SPRMIB is requested.
+    else:
+         subfunction = int(subfun)   
+    req_bytes = [sid, subfunction]
+    print(f"{sid} {subfunction}")
+    return(req_bytes)
+
+def form_reqmsg4srv19_subfun_14(sprmib_flag): 
+    sid = int("19", 16)
+    subfun=int("14",16)
+    if (sprmib_flag == True):
+         subfunction = int(subfun) | 0x80   #MSB is set if SPRMIB is requested.
+    else:
+         subfunction = int(subfun)   
+    req_bytes = [sid, subfunction]
+    print(f"{sid} {subfunction}")
+    return(req_bytes)
+
+def form_reqmsg4srv19_subfun_15(sprmib_flag): 
+    sid = int("19", 16)
+    subfun=int("15",16)
+    if (sprmib_flag == True):
+         subfunction = int(subfun) | 0x80   #MSB is set if SPRMIB is requested.
+    else:
+         subfunction = int(subfun)   
+    req_bytes = [sid, subfunction]
+    print(f"{sid} {subfunction}")
+    return(req_bytes)
+
+def form_reqmsg4srv19_subfun_16(DTCExtDataRecordNumber,sprmib_flag): 
+    sid = int("19", 16)
+    subfun=int("16",16)
+    if (sprmib_flag == True):
+         subfunction = int(subfun) | 0x80   #MSB is set if SPRMIB is requested.
+    else:
+         subfunction = int(subfun)   
+    DTCExtDataRecordNumber_without_spaces = re.sub(r"\s+", "", DTCExtDataRecordNumber)
+    DTCExtDataRecordNumber_hex = int(DTCExtDataRecordNumber, 16)
+    req_bytes = [sid, subfunction, DTCExtDataRecordNumber_hex]
+    print(f"{sid} {subfunction} {DTCExtDataRecordNumber_hex}")
+    return(req_bytes)
+
+def form_reqmsg4srv19_subfun_18(DTCMaskRecord, DTCSnapshotRecordNumber, MemorySelection, sprmib_flag): 
+    sid = int("19", 16)
+    subfun=int("18",16)
+    if (sprmib_flag == True):
+         subfunction = int(subfun) | 0x80   #MSB is set if SPRMIB is requested.
+    else:
+         subfunction = int(subfun)   
+    DTCMaskRecord_without_spaces = re.sub(r"\s+", "", DTCMaskRecord)
+    DTC_highbyte = int(DTCMaskRecord_without_spaces[:2], 16)
+    DTC_middlebyte = int(DTCMaskRecord_without_spaces[2:4], 16)
+    DTC_lowbyte = int(DTCMaskRecord_without_spaces[4:], 16)
+    DTCSnapshotRecordNumber_without_spaces = re.sub(r"\s+", "", DTCSnapshotRecordNumber)
+    DTCSnapshotRecordNumber_hex = int(DTCSnapshotRecordNumber_without_spaces, 16)
+    MemorySelection_without_spaces = re.sub(r"\s+", "", MemorySelection)
+    MemorySelection_hex = int(MemorySelection_without_spaces, 16)
+    req_bytes = [sid, subfunction, DTC_highbyte,DTC_middlebyte,DTC_lowbyte,DTCSnapshotRecordNumber_hex,MemorySelection_hex]
+    print(f"{sid} {subfunction} {DTC_highbyte} {DTC_middlebyte} {DTC_lowbyte} {DTCSnapshotRecordNumber_hex} {MemorySelection_hex}")
+    return(req_bytes)
+
+def form_reqmsg4srv19_subfun_19(DTCMaskRecord, DTCExtDataRecordNumber, MemorySelection, sprmib_flag): 
+    sid = int("19", 16)
+    subfun=int("19",16)
+    if (sprmib_flag == True):
+         subfunction = int(subfun) | 0x80   #MSB is set if SPRMIB is requested.
+    else:
+         subfunction = int(subfun)   
+    DTCMaskRecord_without_spaces = re.sub(r"\s+", "", DTCMaskRecord)
+    DTC_highbyte = int(DTCMaskRecord_without_spaces[:2], 16)
+    DTC_middlebyte = int(DTCMaskRecord_without_spaces[2:4], 16)
+    DTC_lowbyte = int(DTCMaskRecord_without_spaces[4:], 16)
+    DTCExtDataRecordNumber_without_spaces = re.sub(r"\s+", "", DTCExtDataRecordNumber)
+    DTCExtDataRecordNumber_hex = int(DTCExtDataRecordNumber_without_spaces, 16)
+    MemorySelection_without_spaces = re.sub(r"\s+", "", MemorySelection)
+    MemorySelection_hex = int(MemorySelection_without_spaces, 16)
+    req_bytes = [sid, subfunction, DTC_highbyte,DTC_middlebyte,DTC_lowbyte,DTCExtDataRecordNumber_hex,MemorySelection_hex]
+    print(f"{sid} {subfunction} {DTC_highbyte} {DTC_middlebyte} {DTC_lowbyte} {DTCExtDataRecordNumber_hex} {MemorySelection_hex}")
+    return(req_bytes)
+
+def form_reqmsg4srv19_subfun_42(FunctionalGroupIdentifier, DTCSeverityMaskRecord, sprmib_flag): 
+    sid = int("19", 16)
+    subfun=int("42",16)
+    if (sprmib_flag == True):
+         subfunction = int(subfun) | 0x80   #MSB is set if SPRMIB is requested.
+    else:
+         subfunction = int(subfun)   
+    FunctionalGroupIdentifier_without_spaces = re.sub(r"\s+", "", FunctionalGroupIdentifier)
+    FunctionalGroupIdentifier_hex = int(FunctionalGroupIdentifier_without_spaces, 16)
+    DTCSeverityMaskRecord_without_spaces = re.sub(r"\s+", "", DTCSeverityMaskRecord)
+    DTC_highbyte = int(DTCSeverityMaskRecord_without_spaces[:2], 16)
+    DTC_lowbyte = int(DTCSeverityMaskRecord_without_spaces[2:], 16)
+    req_bytes = [sid, subfunction, DTC_highbyte,DTC_lowbyte,FunctionalGroupIdentifier_hex]
+    print(f"{sid} {subfunction} {DTC_highbyte} {DTC_lowbyte} {FunctionalGroupIdentifier_hex}")
+    return(req_bytes)
+
+def form_reqmsg4srv19_subfun_55(FunctionalGroupIdentifier, sprmib_flag): 
+    sid = int("19", 16)
+    subfun=int("55",16)
+    if (sprmib_flag == True):
+         subfunction = int(subfun) | 0x80   #MSB is set if SPRMIB is requested.
+    else:
+         subfunction = int(subfun)   
+    FunctionalGroupIdentifier_without_spaces = re.sub(r"\s+", "", FunctionalGroupIdentifier)
+    FunctionalGroupIdentifier_hex = int(FunctionalGroupIdentifier_without_spaces, 16)
+    req_bytes = [sid, subfunction, FunctionalGroupIdentifier_hex]
+    print(f"{sid} {subfunction} {FunctionalGroupIdentifier_hex}")
+    return(req_bytes)
 
 if __name__ == "__main__":
      print(form_reqmsg4srv19_subfun_1_2_0F_11_12_13("0A","0A",False))
